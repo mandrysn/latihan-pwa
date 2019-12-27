38adcd9820fe5cbcebbf04de045add43ecc8d58e
mandrysn <mandryseptiannur@yahoo.com>
2019-12-27 12:16:53 +0800

    submission

---
+++ submission/js/nav.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // sidebar nav
+  var elems = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(elems);
+  loadNav();
+ 
+  function loadNav() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.status != 200) return;
+   
+        // tampilkan link menu
+        document.querySelectorAll(".topnav, .sidenav").forEach(function(elm) {
+          elm.innerHTML = xhttp.responseText;
+        });
+   
+        // daftarkan event listener untuk setiap tautan menu agar bisa di klik
+        document.querySelectorAll(".sidenav a, .topnav a").forEach(function(elm) {
+          elm.addEventListener("click", function(event) {
+            // tutup menu mobile
+            var sidenav = document.querySelector(".sidenav");
+            M.Sidenav.getInstance(sidenav).close();
+   
+            // tampilkan isi halaman yang dipanggil
+            page = event.target.getAttribute("href").substr(1);
+            loadPage(page);
+          });
+        });
+      }
+    };
+    xhttp.open("GET", "nav.html", true);
+    xhttp.send();
+  }
+
+  // load page content
+  var page = window.location.hash.substr(1); // mengambil hash dari url halaman
+  if (page == "") page = "home"; // halaman default home yang akan ditampilkan bila hash tidak ditemukan
+  loadPage(page); 
+  
+  function loadPage(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        var content = document.querySelector("#body-content");
+        if (this.status == 200) {
+          content.innerHTML = xhttp.responseText;
+        } else if (this.status == 404) {
+          content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+        } else {
+          content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+        }
+      }
+    };
+    xhttp.open("GET", "pages/" + page + ".html", true);
+    xhttp.send();
+  }
+});
